abdd6c6c7c47bcb91541deac1f0718ddc35b3ac1
fedokanishey <kanishey@gmail.com>
2026-01-21 18:32:32 +0200

    edit error

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -19,7 +19,7 @@ export async function generateMetadata({
     };
   }
 
-  const displayName = portfolio.content.name || (portfolio.userId as { name: string })?.name || "Portfolio";
+  const displayName = portfolio.content.displayName || (portfolio.userId as { name: string })?.name || "Portfolio";
 
   return {
     title: `${displayName} | Portfolio`,
